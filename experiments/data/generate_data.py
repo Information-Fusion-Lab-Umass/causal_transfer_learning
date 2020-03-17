f4792497ec2045772d4d2c781ea361a3c1fe56e8
purvapruthi <purvapruthi617@gmail.com>
2020-03-17 15:35:57 -0400

    Added data generation module

---
+++ experiments/data/generate_data.py
@@ -0,0 +1,70 @@
+import argparse
+import gym
+from mazelab.generators import basic_maze
+from mazelab.envs import SourceEnv
+import time
+import numpy as np
+from oo_representation import *
+
+parser = argparse.ArgumentParser("Arguments for environment generation for causal concept understanding")
+
+parser.add_argument('--env', default="source", help='type of environment')
+parser.add_argument('--n_episodes', default=300, type = int, metavar = "N", help='number of episodes')
+parser.add_argument('--n_len', default=500, type = int, metavar = "N", help='number of steps in each episode')
+
+args = parser.parse_args()
+
+def main():
+    # switch_positions = [[2,2], [4,6]]
+    # prize_positions = [[7,6],[5,5]]
+    switch_positions = []
+    prize_positions = []
+    x = basic_maze(width=10, height = 10, switch_positions = switch_positions, prize_positions = prize_positions)
+    start_idx = [[7, 2]]
+    env_id = 'SourceMaze-v0'
+
+    gym.envs.register(id = env_id, entry_point = SourceEnv, max_episode_steps = 1000)
+
+    if args.env == "source":
+        invert = False
+
+    if args.env == "target":
+        invert = True
+
+    env = gym.make(env_id, x = x, start_idx = start_idx, invert = invert, return_image = True)
+    n_actions = env.action_space.n
+    epsilon = 1.0
+    empty_positions = env.maze.objects.free.positions
+    count = 0
+    inp = None
+    n_colors = len(env.maze.objects)
+    for p in empty_positions:
+        start_idx = [p]
+        env = gym.make(env_id, x = x, start_idx = start_idx, free_positions = empty_positions, invert = invert)
+        for j in range(n_actions):
+            curr_obs = env.reset()
+            curr_objects = env.maze.objects
+            env.render()
+            # time.sleep(0.1)
+            action = j
+            X, colors_dict = get_oo_repr(count, curr_objects, action, n_colors, n_actions)
+            if inp is None:
+                N, M = X.shape
+                inp = np.zeros((480 + 32, N, M))
+            inp[count, :, :] = X
+            count = count + 1
+            # next state
+            next_obs, reward, done, info = env.step(action)
+            next_objects = env.maze.objects
+            # rewards
+            env.render()
+            X, colors_dict = get_oo_repr(count, next_objects, action, n_colors, n_actions)
+            inp[count, :, :] = X
+            count = count + 1
+            if done:
+                break
+        env.close()
+    np.savez("./mat/oo_transition_matrix.npz", mat = inp, c_dict = [colors_dict])
+
+if __name__ == "__main__":
+    main()
---
+++ experiments/data/oo_representation.py
@@ -0,0 +1,64 @@
+import numpy as np
+
+def get_neighboring_objects(X, action_ohe):
+    Y = []
+    idx = 1
+    for i in range(X.shape[0]):
+        color = X[i,0].decode("utf-8")
+        if color not in ["black", "white", "red", "green"]:
+            features = []
+            features.append(X[i,idx:])
+            for j in range(X.shape[0]):
+                if i != j:
+                    x_diff = int(X[i,2]) - int(X[j,2])
+                    y_diff = int(X[i,3]) - int(X[j,3])
+                    if y_diff == -1 and x_diff == 0:
+                        r = X[j,idx+1:]
+
+                    if y_diff == 1 and x_diff == 0:
+                        l = X[j,idx+1:]
+
+                    if x_diff == -1 and y_diff == 0:
+                        d = X[j,idx+1:]
+
+                    if x_diff == 1 and y_diff == 0:
+                        u = X[j,idx+1:]
+            features.append(l)
+            features.append(r)
+            features.append(d)
+            features.append(u)
+            features.append(action_ohe)
+
+            flat_list = []
+            for sublist in features:
+                for item in sublist:
+                    flat_list.append(item)
+            Y.append(flat_list)
+    Y = np.asarray(Y).astype("float32")
+    return Y
+
+def get_oo_repr(t, objects, action, n_colors, n_actions):
+    A = []
+    for o in objects:
+        for p1, p2 in o.positions:
+                A.append([o.colorname, p1, p2])
+
+    A = np.asarray(A)
+    colors = A[:,0]
+    uniq_colors, colors_int = np.unique(colors, return_inverse=True)
+    colors_dict = {}
+    for i in range(len(colors)):
+        colors_dict[colors_int[i]] = colors[i]
+    X = np.zeros((A.shape[0], n_colors + 4), dtype=np.dtype('a16'))
+
+    X[:, 0] = colors
+    X[:, 1] = np.ones(A.shape[0])* t
+    X[:, 2] = A[:, 1]
+    X[:, 3] = A[:, 2]
+    X[:, 4: 4+n_colors] = one_hot(colors_int, n_colors)
+    action_ohe = one_hot(np.ones(1, dtype = "int")* int(action), n_actions)
+    nbrs = get_neighboring_objects(X, action_ohe)
+    return nbrs, colors_dict
+
+def one_hot(a, num_classes):
+    return np.squeeze(np.eye(num_classes)[a.reshape(-1)])
