333e246b2a6d93858060a9ca36a7c60b5766a060
purvapruthi <purvapruthi617@gmail.com>
2020-03-18 20:00:08 -0400

    Added model for relational reasoning

--- codes/models/relation_learning/simple_nn.py
+++ codes/models/relation_learning/simple_nn.py
@@ -2,9 +2,12 @@ import torch
 from torch import nn as nn
 from torch import optim as optim
 from torch.nn import functional as F
+from torch.utils.tensorboard import SummaryWriter
+import logging
+from codes.utils import analyze
 
 class RelationalNN(nn.Module):
-    def __init__(self, x_dim, h_dim, y_dim):
+    def __init__(self, x_dim, h_dim, y_dim, c_dict):
         super(RelationalNN, self).__init__()
         self.x_dim = x_dim
         self.h_dim = h_dim
@@ -12,7 +15,12 @@ class RelationalNN(nn.Module):
         self.fc1 = nn.Linear(x_dim, h_dim)
         self.fc2 = nn.Linear(h_dim, y_dim)
         self.optimizer = optim.Adam(self.parameters(), lr = 1e-2, weight_decay = 1e-4)
-
+        format = "%(asctime)s.%(msecs)03d: - %(levelname)s: %(message)s"
+        logging.basicConfig(format=format, level=logging.INFO,
+        datefmt="%H:%M:%S")
+        self.logger = logging.getLogger(__name__)
+        self.writer = SummaryWriter()
+        self.c_dict = c_dict
 
     def forward(self, x):
         x1 = F.relu(self.fc1(x))
@@ -24,22 +32,23 @@ class RelationalNN(nn.Module):
         return mse_loss(y_pred, y)
 
     def train(self, epoch, xtr, ytr):
-        print("========================== Train =====================")
+        self.logger.info("========================== Train =====================")
 
         self.optimizer.zero_grad()
         ytr_pred = self.forward(xtr)
         train_loss = self.loss(ytr_pred, ytr)
 
         train_loss.backward()
-
-        print('====> Epoch {}, Train set loss: {:.4f}'.format(epoch, train_loss.item()))
+        self.writer.add_scalar('Loss/train', train_loss.item(), epoch)
+        self.logger.info('====> Epoch {}, Train set loss: {:.4f}'.format(epoch, train_loss.item()))
         self.optimizer.step()
 
     def test(self, epoch, xte, yte):
-        print("========================== Test =====================")
+        self.logger.info("========================== Test =====================")
 
         with torch.no_grad():
             yte_pred = self.forward(xte)
-            print(xte[:5], yte_pred[:5], yte[:5])
             test_loss = self.loss(yte_pred, yte)
-            print('====> Epoch {}, Test set loss: {:.4f}'.format(epoch, test_loss.item()))
+            self.writer.add_scalar('Loss/test', test_loss.item(), epoch)
+            print(analyze(xte[:5], self.c_dict), yte[:5], yte_pred[:5])
+            self.logger.info('====> Epoch {}, Test set loss: {:.4f}'.format(epoch, test_loss.item()))
--- codes/utils/util_fns.py
+++ codes/utils/util_fns.py
@@ -7,19 +7,20 @@ actions = {
 }
 
 def analyze(inp, colors_dict):
-    result = np.zeros((inp.shape[0], 17), dtype=np.dtype('a16'))
+    if not isinstance(inp, np.ndarray):
+        inp = inp.detach().numpy()
+    result = np.zeros((inp.shape[0], 16), dtype=np.dtype('a16'))
     for i in range(len(inp)):
-        result[i, 0] = inp[i,0]
         idx = 0
-        count = 1
+        count = 0
         for j in range(5):
-            result[i, count] = inp[i, idx + 1]
-            result[i, count + 1] = inp[i, idx + 2]
-            color_ohe = decode_onehot(inp[i,idx + 3: idx + 8])
+            result[i, count] = inp[i, idx]
+            result[i, count + 1] = inp[i, idx + 1]
+            color_ohe = decode_onehot(inp[i,idx + 2: idx + 7])
             result[i, count + 2] = colors_dict[color_ohe]
             idx = idx + 7
             count = count + 3
-        result[i, -1] = actions[decode_onehot(inp[i, 36:40])]
+        result[i, -1] = actions[decode_onehot(inp[i, 35:39])]
     return result
 
 def decode_onehot(a):
