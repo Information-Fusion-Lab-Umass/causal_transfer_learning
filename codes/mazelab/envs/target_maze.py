4e67676ecfa64d06f6ed723e9be217a3a2f10e7e
purvapruthi <purvapruthi617@gmail.com>
2020-05-20 14:39:09 -0400

    Activated target env

--- codes/mazelab/envs/target_maze.py
+++ codes/mazelab/envs/target_maze.py
@@ -13,11 +13,11 @@ class TargetMaze(BaseMaze):
         return self.x.shape
 
     def make_objects(self):
-        free = Object('free', 0, color.free, False, np.stack(np.where(self.x == 0), axis = 1))
-        obstacle = Object('obstacle', 1, color.obstacle, True, np.stack(np.where(self.x == 1), axis = 1))
+        free = Object('free', 0, color.free, "white", False, np.stack(np.where(self.x == 0), axis = 1))
+        obstacle = Object('obstacle', 1, color.obstacle, "black", True, np.stack(np.where(self.x == 1), axis = 1))
 
         #inverted positions and colors of switch and prizes
-        switch = Object('switch', 3, color.prize, True, np.stack(np.where(self.x == 3), axis = 1))
-        prize = Object('prize', 2, color.switch, True, np.stack(np.where(self.x == 2), axis = 1))
-        agent = Object('agent', 4, color.agent, False, [])
+        switch = Object('switch', 3, color.prize, "red", True, np.stack(np.where(self.x == 3), axis = 1))
+        prize = Object('prize', 2, color.switch, "green", True, np.stack(np.where(self.x == 2), axis = 1))
+        agent = Object('agent', 4, color.agent, "yellow", False, np.stack(np.where(self.x == 4), axis = 1))
         return free, obstacle, switch, prize, agent
