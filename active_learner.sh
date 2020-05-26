82425f0c100b672b9935109caaf8ad3343af125b
purvapruthi <purvapruthi617@gmail.com>
2020-05-26 17:28:22 -0400

    Cleaning

--- active_learner.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd ~/research/research-master/causal_transfer_learning
-for (( i=10; i <=100; i = i + 10 ))
-do
-	PYTHONPATH=$PWD python codes/data/test_notears_nonlinear.py --game_type all_random --l1 0.01 --l2 0.01 --rho 1 --mode both --disp False --train_frac $i --save_results 1
-done
