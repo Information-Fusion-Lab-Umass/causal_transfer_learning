ee833c65571c3768308e3af5a3e597de22e06636
purvapruthi <purvapruthi617@gmail.com>
2020-06-06 00:53:29 -0400

    pushed analyzed data

--- codes/data/analyze_data.py
+++ codes/data/analyze_data.py
@@ -7,7 +7,7 @@ import os
 parser = argparse.ArgumentParser("Arguments for analyzing the environment for causal concept understanding")
 parser.add_argument("--start", type = int, required = True, help = "Starting height of environment")
 parser.add_argument("--stop", type = int, required = True, help = "Maximum height of the environment")
-parser.add_argument('--game_type', default = "bw", choices = ["bw", "all_random_invert", "all_random", "trigger_non_markov"], help = "Type of game", required = True)
+parser.add_argument('--game_type', default = "bw", choices = ["bw", "all_random_invert", "all_random", "trigger_non_markov", "trigger_non_markov_random"], help = "Type of game", required = True)
 args = parser.parse_args()
 np.printoptions(scientific = False)
 
@@ -51,22 +51,12 @@ print(c_dict)
 #
 for i in range(4):
     action_data = all_data[all_data[:, 16] == i]
-    print("Action {}".format(actions[i]))
-    print(action_data[:4])
     s_form = structural_form(action_data)
-    print(s_form[:2])
-#     # np.savetxt(current_dir + "mat/R/oo_s_form_{}.csv".format(i), s_form[:20], delimiter = ",")
-#     np.savez(data_dir + "oo_action_{}_{}.npz".format(i, args.game_type), mat = s_form, c_dict = [c_dict])
-# # #
+    np.savetxt(data_dir + "oo_s_form_{}.csv".format(i), s_form[:20], delimiter = ",")
+    np.savez(data_dir + "oo_action_{}_{}.npz".format(i, args.game_type), mat = s_form, c_dict = [c_dict])
+
 # # # names = ['ax_t1', 'ay_t1', 'ac_t1', 'ux_t1', 'uy_t1', 'uc_t1', 'dx_t1',
 # # 'dy_t1', 'dc_t1', 'rx_t1', 'ry_t1', 'rc_t1', 'lx_t1', 'ly_t1', 'lc_t1', 'a_t1',
 # # 'ax_t2', 'ay_t2', 'ac_t2', 'ux_t2', 'uy_t2', 'uc_t2', 'dx_t2',
 # # 'dy_t2', 'dc_t2', 'rx_t2', 'ry_t2', 'rc_t2', 'lx_t2', 'ly_t2', 'lc_t2', 'a_t2']
 #
-# # values = [ np.max(result[:,i]) for i in range(result.shape[1])]
-# # discrete = ['d' for i in range(len(names))]
-# # header = np.array([names, values, discrete])
-# # np.savetxt(current_dir + "mat/R/oo_transition_matrix.csv", result, delimiter = ",")
-#
-# # np.savetxt(current_dir + "mat/R/header.csv", header, delimiter = "," , fmt="%s")
-# # np.savez(current_dir + "mat/oo_transition_matrix.npz", mat = all_data, c_dict = [c_dict])
